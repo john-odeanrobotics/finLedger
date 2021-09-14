89fca85c9f262ef64cd4540d1fd4ffe0d6c03cbf
john <john@odeanrobotics.com>
2021-09-14 15:31:24 +0900

    add unique constraint

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,13 @@
 module.exports = (sequelize, Sequelize) => {
     const User = sequelize.define("user", {
         uid : {
-            type: Sequelize.STRING(20)
+            type: Sequelize.STRING(20),
+            allowNull: false,
+            unique: true
         },
         password: {
-            type: Sequelize.STRING(20)
+            type: Sequelize.STRING(20),
+            allowNull: false,
         }
     });
     
